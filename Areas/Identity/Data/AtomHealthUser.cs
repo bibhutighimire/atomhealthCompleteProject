9686875354bbaed2b13d003be1f1016d26d2d8a7
shailza85 <33739384+shailza85@users.noreply.github.com>
2020-11-30 11:04:39 -0700

    Add project files.

---
+++ Areas/Identity/Data/AtomHealthUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace AtomHealth.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the AtomHealthUser class
+    public class AtomHealthUser : IdentityUser
+    {
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string FirstName { get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string MiddleName { get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string LastName { get; set; }
+    }
+}
---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,35 @@
+﻿using AtomHealth.Areas.Identity.Data;
+using AtomHealth.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AtomHealth.Areas.Identity.IdentityHostingStartup))]
+namespace AtomHealth.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AtomHealthDBContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AtomHealthDBContextConnection")));
+
+                
+                services.AddIdentity<AtomHealthUser,IdentityRole>(options => {
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    options.SignIn.RequireConfirmedAccount = true;
+                })
+                 .AddDefaultTokenProviders()
+                .AddDefaultUI()
+                    .AddEntityFrameworkStores<AtomHealthDBContext>();
+
+
+            });
+        }
+    }
+}
